ef97c627d75f23fdf037a4dc6471f9901f9dc406
zwd <wd18535470628@hotmail.com>
2018-02-05 11:15:08 +0800

    斐波那契数列实现

--- src/com/fibonacci/Test/Test.java
+++ src/com/fibonacci/Test/Test.java
@@ -1,16 +1,20 @@
 package com.fibonacci.Test;
 
+import java.util.Iterator;
+
 public class Test {
 	
 	
-	//1，1，2，3，5，8，13，21,34,55,89，以此类推，后一项为前两项的和
+	//0,1,1,2,3,5,8,13,21,34,55,89，以此类推，后一项为前两项的和 F(0)=0，F(1)=1,F(2)=1, F(n)=F(n-1)+F(n-2)（n>=2，n∈N*）
 	public static int fibonacci(int n) {  
-	    if (n <= 0) {  
+	    if (n < 0) {  
 	        return -1;  
-	    } else if (n == 1 || n == 2) {  
+	    } else if (n == 0) {
+			return 0;
+		}else if (n == 1 || n == 2) {  
 	        return 1;  
 	    } else {  
-	        int flag = 3;  
+	        int flag = 3;//从第三位开始  
 	        int f = 1;//第1位  
 	        int b = 1;//第2位  
 	        while (flag++ <= n) {  
@@ -31,8 +35,10 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int number = fibonacci(13);
-		System.out.println("number:" + number);
+		for (int i = 0; i < 30; i++) {
+			int number = fibonacci(i);
+			System.out.println("第 " + i + "项: " + number);
+		}
 	}
 
 }
